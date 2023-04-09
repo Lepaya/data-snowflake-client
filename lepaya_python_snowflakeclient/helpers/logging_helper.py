e8b7eb2ff4a368ec71cf9b08879d78eb5f3a68ca
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2023-04-09 18:57:49 +0200

    bug fixes

--- lepaya_python_snowflakeclient/helpers/logging_helper.py
+++ lepaya_python_snowflakeclient/helpers/logging_helper.py
@@ -12,23 +12,6 @@ LOGGER = structlog.get_logger()
 PACKAGE_NAMES = ["requests", "urllib3", "snowflake.connector", "slack_sdk"]
 
 
-def set_logger_level(log_level: int) -> None:
-    """
-    Set a logger level for the project.
-
-        - Set the logging level for the project.
-        - Skip unnecessary warnings.
-
-    Args:
-         log_level: logging level for the project.
-    """
-    for name in PACKAGE_NAMES:
-        logging.getLogger(name).setLevel(log_level)
-
-    # Skip unnecessary warnings
-    os.environ["SKIP_SLACK_SDK_WARNING"] = "True"
-
-
 def log_and_update_slack(slack_client: SlackClient, message: str, temp: bool) -> None:
     """
     Log a response and update slack.
