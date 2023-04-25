3afb2aabaf7d7362faf914486375323979e035b7
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2023-04-26 00:39:20 +0200

    optimize imports

--- data_snowflake_client/snowflake_client.py
+++ data_snowflake_client/snowflake_client.py
@@ -2,13 +2,12 @@
 from __future__ import annotations
 
 import pandas as pd
-import structlog
 import snowflake.connector
+from data_slack_client.slack_client import SlackClient
 from snowflake.connector import SnowflakeConnection
 from snowflake.connector.errors import DatabaseError
 from snowflake.connector.pandas_tools import write_pandas
 
-from data_slack_client.slack_client import SlackClient
 from .helpers.logging_helper import log_and_raise_error, log_and_update_slack
 from .models.config_model import SnowflakeConfig
 
