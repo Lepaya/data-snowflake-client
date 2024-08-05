290b55b8292f1e08d6de05f98e86614d80bc35c4
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2024-08-05 16:07:32 +0200

    fix pre-commit

--- data_snowflake_client/utils/encoding.py
+++ data_snowflake_client/utils/encoding.py
@@ -18,6 +18,7 @@ def encode_data_base64(text: str) -> str:
     base64_bytes = base64.b64encode(text_bytes)
     return base64_bytes.decode("ascii")
 
+
 def decode_data_base64(text: str) -> str:
     """
     Decode a base64 encoded string.
@@ -32,11 +33,12 @@ def decode_data_base64(text: str) -> str:
     text_bytes = base64.b64decode(base64_bytes)
     return text_bytes.decode("ascii")
 
+
 def convert_private_key_to_der(private_key: str) -> bytes:
     """
     Convert a private key in string format to DER format.
 
-    DER (Distinguished Encoding Rules) is a binary format used to encode 
+    DER (Distinguished Encoding Rules) is a binary format used to encode
     cryptographic keys and certificates.
 
     Args:
@@ -45,19 +47,17 @@ def convert_private_key_to_der(private_key: str) -> bytes:
     Returns:
         bytes: The private key in DER format.
     """
-    
+
     # Load the PEM-encoded private key
     private_key_obj = serialization.load_pem_private_key(
-        private_key.encode("utf-8"),
-        password=None,
-        backend=default_backend()
+        private_key.encode("utf-8"), password=None, backend=default_backend()
     )
-    
+
     # Convert the private key to DER format
     private_key_der = private_key_obj.private_bytes(
         encoding=serialization.Encoding.DER,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+        encryption_algorithm=serialization.NoEncryption(),
     )
-    
+
     return private_key_der
